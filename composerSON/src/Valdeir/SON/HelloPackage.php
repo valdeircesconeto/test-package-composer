803350ae2053df5c0900ee0d0f911f54ee110035
valdeir cesconeto <valdeircesconetto@gmail.com>
2017-08-03 20:53:39 -0400

    file test composer

---
+++ composerSON/src/Valdeir/SON/HelloPackage.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valdeir
+ * Date: 03/08/17
+ * Time: 00:23
+ */
+
+namespace Valdeir\SON;
+
+class HelloPackage{
+
+
+    public function getHello(){
+            return "Hello Package";
+    }
+}
